11b6f8f0051d5264f8589147bb1a38f87b3ada2d
David Persson <daviderikpersson@gmail.com>
2016-01-26 21:11:02 -0600

    Added ExampleBeer

--- application/controllers/Beer.php
+++ application/controllers/Beer.php
@@ -14,6 +14,11 @@ class Beer extends MY_Controller {
                           
         $this->sendResponse(200, ['results' => $query->result_array()]);
     }
+    public function ExampleBeer() {
+        $query = $this->db->where('Beer_id', "12")
+                          ->get('Beers');
+        $this->sendResponse(200, ['results' => $query->result_array()]);
+    }
     public function Ale() {
         $query = $this->db->where('Type', "Ale")
                           ->get('Beers');         
