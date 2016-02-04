f5ec3e15563c020fa0c4b28b1b23b41cae43a063
David Persson <daviderikpersson@gmail.com>
2016-02-03 18:23:40 -0600

    changed new_search index -> search

--- application/controllers/new_search.php
+++ application/controllers/new_search.php
@@ -5,7 +5,7 @@ class New_search extends MY_Controller {
     public function __construct() {
         parent::__construct();
     }
-    public function index(){
+    public function search(){
 
         // Load required models
         //$this->load->model('search_m');
