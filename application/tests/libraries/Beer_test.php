24bbedef1d67c14fcf8b49575f615139baf85ae9
colindignazio <colin.dignazio@gmail.com>
2016-02-05 03:05:20 -0600

    Beer tests

--- application/tests/libraries/Beer_test.php
+++ application/tests/libraries/Beer_test.php
@@ -6,3 +6,24 @@ class Beer_test extends TestCase {
         $this->CI->load->library('Beer_lib', array(0 => 'Beer_access_stub'));
         $this->obj = $this->CI->beer_lib;
     }
+
+    public function test_getBeerById() {
+        $output = $this->obj->getBeerById(1);
+        $this->assertEquals('Ale', $output[0]['Type']);
+        $this->assertEquals('Grasshopper', $output[0]['Name']);
+        $this->assertEquals('5', $output[0]['Alcohol_By_Volume']);
+        $this->assertEquals('Big Rock', $output[0]['Brewery']);
+
+        $output = $this->obj->getBeerById(100);
+        $this->assertEquals(null, $output);
+    }
+
+    public function test_getSearchResults() {
+
+    }
+
+    public function test_getAllBeers() {
+        $output = $this->obj->getAllBeers();
+        $this->assertEquals(3, count($output));
+    }
+}
