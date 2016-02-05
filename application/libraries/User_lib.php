8aeead8a12e788d0d4ef74092ac1aaad6111f83d
Mitchell Hamm <mitchellphamm@hotmail.com>
2016-02-05 03:00:01 -0600

    code refactor yay, everything is in the library now.

--- application/libraries/User_lib.php
+++ application/libraries/User_lib.php
@@ -5,6 +5,128 @@ class User_lib {
 	public function __construct($dbaccess = array(0 => 'User_access_object')) {
 		$this->CI =& get_instance();
         $this->CI->load->model($dbaccess[0], 'user_access');
+        $this->CI->load->model('sessions');
+    }
+
+    public function createAccount($username, $password, $email, $location) {
+        $userInfoError = $this->validateUserInfo($username, $password, $email, $location);
+
+        if(is_null($userInfoError)) {
+            $emailCode = genVerificationCode();
+            // Insert row into users table
+            $data = [
+                'User_name'            => $username,
+                'User_password'        => password_hash($password, PASSWORD_DEFAULT),
+                'User_email'           => $email,
+                'User_location'        => $location,
+                'User_email_code'      => $emailCode
+            ];
+
+            // Insert and check for unique key error
+            if(!$this->CI->user_access->createAccount($data)) {
+                $result = ['status' => 500, 'details' => 'An unknown error occurred'];
+            }
+            else {
+                $this->CI->load->library('emailer');
+                $this->CI->emailer->sendVerificationCode($params['email'], $emailCode);
+                $userId = $this->CI->user_access->insert_id();
+                $sessionId = $this->CI->sessions->createSession($userId);
+                unset($data['User_password']);
+                unset($data['User_email_code']);
+                $result = ['status' => 200, 'user' => $data, 'sessionId' => $sessionId];
+            }
+        } 
+        else {
+            $result = ['status' => 400, 'details' => $userInfoError];
+        }
+        return $result;
+    }
+
+    public function login($username, $password) {
+        $query = $this->CI->user_access->getUserByName($username);
+        if(count($query->result_array()) > 0) {
+            $user = $query->row_array();
+
+            if(password_verify($password, $user['User_password'])) {
+                unset($user['User_password']);
+                $sessionId = $this->CI->sessions->createSession($user['User_id']);
+                $result['status'] = 200;
+                $result['user'] = $user;
+                $result['sessionId'] = $sessionId;
+            } else {
+                $result['status'] = 400;
+                $result['details'] = 'Invalid username or password';
+            }
+        } else {
+            $result['status'] = 400;
+            $result['details'] = 'Invalid username or password';
+        }
+        return $result;
+    }
+
+    public function logout($sessionId) {
+        if(! $this->CI->user_access->logout($sessionId)) {
+            $result = ['status' => 400, 'details' => 'Error logging out'];
+        } else {
+            $result['status'] = 200;
+        }
+        return $result;
+    }
+
+    public function setUsername($sessionId, $userName) {
+        $data = ['User_name'    => $userName];
+
+        if(FALSE !== $user = $this->CI->sessions->getUser($sessionId)) {
+
+            if(!$this->CI->user_access->update($user['User_id'], $data)) {
+                $error = $this->CI->user_access->lastError();
+
+                if(1062 == $error['code']) {
+                    $result = ['status' => 400, 'details' => 'Username address in use'];
+                }
+                else {
+                    $result = ['status' => 500, 'details' => 'An unknown error occurred'];
+                }
+            } else {
+                $result['status'] = 200;
+            }
+        } else {
+            $result['status'] = 401;
+        }
+        return $result;
+    }
+
+    public function setPassword($sessionId, $oldPass, $newPass) {
+        if(FALSE !== $user = $this->CI->sessions->getUser($sessionId)) {
+            $data = ['User_password'    => password_hash($newPass, PASSWORD_DEFAULT)];
+
+            if(password_verify($oldPass, $user['User_password'])) {
+                if(!$this->CI->user_access->update($user['User_id'], $data)) {
+                    $result = ['status' => 500, 'details' => 'An unknown error occurred'];
+                } else {
+                    $result['status'] = 200;
+                }
+            } else {
+                $result = ['status' => 400, 'details' => 'Passwords do not match'];
+            }
+        } else {
+            $result['status'] == 401;
+        }
+        return $result;
+    }
+
+    public function setLocation($sessionId, $location) {
+        if(FALSE !== $user = $this->CI->sessions->getUser($sessionId)) {
+            $data = ['User_location'    => $location];
+            if(!$this->CI->user_access->update($user['User_id'], $data)) {
+                $result = ['status' => 500, 'details' => 'An unknown error occurred'];
+            } else {
+                $result = ['status' => 200];
+            }
+        } else {
+            $result = ['status' => 401];
+        }
+        return $result;
     }
 
 	public function validateUserInfo($username, $password, $email, $location) {
@@ -37,7 +159,7 @@ class User_lib {
 
         $query = $this->CI->user_access->getUserByName($username);
         
-        if(count($query) > 0) {
+        if(count($query->result_array()) > 0) {
             $result = true;
         }
 
@@ -49,7 +171,7 @@ class User_lib {
 
         $query = $this->CI->user_access->getUserByEmail($email);
             
-        if(count($query) > 0) {
+        if(count($query->result_array()) > 0) {
             $result = true;
         }
 
