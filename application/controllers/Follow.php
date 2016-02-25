81508e8ea809c1ab673da11b0deb1175d68cf3d5
colindignazio <colin.dignazio@gmail.com>
2016-02-25 15:19:46 -0600

    Finishing up follow logic and tests

--- application/controllers/Follow.php
+++ application/controllers/Follow.php
@@ -6,4 +6,36 @@ class Follow extends MY_Controller {
         parent::__construct();
         $this->load->library('follow_lib');
     }
+
+    public function followUser() {
+        if(!$this->requireParams(['followerId'  => 'str', 'followeeId'   => 'str'])) return;
+        $params = $this->getParams();
+        $result = $this->follow_lib->followUser($params['followerId'], $params['followeeId']);
+
+        $this->sendResponse($result['status'], ['details' => $result['details']]);
+    }
+
+    public function unfollowUser() {
+        if(!$this->requireParams(['followerId'  => 'str', 'followeeId'   => 'str'])) return;
+        $params = $this->getParams();
+        $result = $this->follow_lib->unfollowUser($params['followerId'], $params['followeeId']);
+
+        $this->sendResponse($result['status'], ['details' => $result['details']]);
+    }
+
+    public function isUserFollowed() {
+        if(!$this->requireParams(['followerId'  => 'str', 'followeeId'   => 'str'])) return;
+        $params = $this->getParams();
+        $result = $this->follow_lib->isUserFollowed($params['followerId'], $params['followeeId']);
+
+        $this->sendResponse($result['status'], ['details' => $result['details']]);  	
+    }
+
+    public function getRecentReviews() {
+        if(!$this->requireParams(['userId'  => 'str'])) return;
+        $params = $this->getParams();
+        $result = $this->follow_lib->getRecentFolloweeReviews($params['followerId']);
+
+        $this->sendResponse($result['status'], ['details' => $result['details']]);
+    }
 }
--- application/tests/libraries/Follow_test.php
+++ application/tests/libraries/Follow_test.php
@@ -8,32 +8,37 @@ class Follow_test extends TestCase {
     }
 
     public function test_getFolloweeCount() {
-    	$this->assertEquals(3, $this->obj->getFolloweeCount(1));
-    	$this->assertEquals(0, $this->obj->getFolloweeCount(0));
-    	$this->assertEquals(0, $this->obj->getFolloweeCount(10));
+    	$this->assertEquals(3, $this->obj->getFolloweeCount(1)['details']);
+    	$this->assertEquals(0, $this->obj->getFolloweeCount(0)['details']);
+    	$this->assertEquals(0, $this->obj->getFolloweeCount(10)['details']);
     }
 
     public function test_getFolloweeNames() {
-    	$names = $this->obj->getFolloweeNames(1);
+    	$names = $this->obj->getFolloweeNames(1)['details'];
 
     	$this->assertEquals("testuser123", $names[0]);
     	$this->assertEquals("Chuffy", $names[1]);
     	$this->assertEquals("Mitchell", $names[2]);
 
-    	$names = $this->obj->getFolloweeNames(0);
+    	$names = $this->obj->getFolloweeNames(0)['details'];
     	$this->assertEquals(0, count($names));
     }
 
     public function test_followUser() {
-    	$this->assertTrue($this->obj->followUser(1, 2));   	
+    	$this->assertEquals('User followed', $this->obj->followUser(1, 2)['details']);   	
     }
 
     public function test_unfollowUser() {
-    	$this->assertTrue($this->obj->unfollowUser(1, 2));   	  	
+    	$this->assertEquals('User unfollowed', $this->obj->unfollowUser(1, 2)['details']);   	  	
     }
 
     public function test_getRecentFollowweeReviews() {
-    	$this->AssertEquals("Decent", $this->obj->getRecentFolloweeReviews(1)[0]);
-    	$this->AssertEquals(0, count($this->obj->getRecentFolloweeReviews(0)));
+    	$this->AssertEquals("Decent", $this->obj->getRecentFolloweeReviews(1)['details'][0]['review']);
+    	$this->AssertEquals(0, count($this->obj->getRecentFolloweeReviews(0)['details']));
+    }
+
+    public function test_isUserFollowed() {
+        $this->AssertTrue($this->obj->isUserFollowed(1, 3)['details']);
+        $this->AssertFalse($this->obj->isUserFollowed(1, 4)['details']);
     }
 }
